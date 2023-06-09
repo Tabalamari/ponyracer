7fba105fb6975e8097faa1123cc0f66953880695
Tabalamari <tabalamari@gmail.com>
2023-06-09 20:50:54 +0100

    Structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RacesComponent } from '../races.component';
 
 @Component({
     selector: 'ns-root',
     standalone: true,
-    imports: [CommonModule],
+    imports: [RacesComponent, CommonModule],
     template: `<h1>PonyRacer</h1>
-                <h2>Welcome {{ user.name }}</h2>`,
-    styleUrls: ['./app.component.css']
+            <div [hidden] = "isHidden">Hidden or not</div>
+            <div hidden = "{{isHidden}}">Hidden or not</div>
+            <p [style.color]="foreground">Friendship is Magic</p>
+
+            <ns-races></ns-races>`,
+    styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
     user = { name: 'Cédric' };
+    isHidden: any;
+    foreground = "blue";
 }
--- src/races.component.ts
+++ src/races.component.ts
@@ -1,8 +1,89 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
-selector: 'ns-races', 
-template: `<h2>Races</h2>`, 
-standalone: true
+    selector: 'ns-races',
+    template: `<div *ngIf="races.length > 0; else empty">
+                    <h2>Races</h2>
+                    <ul>
+                        <li *ngFor="let race of races; index as i">{{i}} - {{ race.name }}</li>
+                    </ul>
+               </div>
+
+    <ng-template #empty><h2>No races.</h2></ng-template>
+    <button (click)="refreshRaces()">Refresh the races list</button>
+    <p>{{ races.length }} races</p>
+    <button (click)="firstName = 'Cédric'; lastName = 'Exbrayat'"> Click to show name Cédric Exbrayat
+    </button>
+
+    <p>{{ firstName }} {{ lastName }}</p>
+    <div [ngSwitch]="messageCount">
+        <p *ngSwitchCase="0">You have no message</p>    
+        <p *ngSwitchCase="1">You have a message</p>
+        <p *ngSwitchDefault>You have some messages</p>
+    </div>
+    <div (click)="onButtonClick($event)"> <button>Click me!</button>
+    <textarea (keydown.space)="onSpacePress()">Press space!</textarea>
+    <textarea (keydown.space)="onSpacePress1($event)">Press space!</textarea>
+    </div>
+
+<div style = "margin: 10px; padding: 10px; background-color: blue" (click)="bubbling()"> 
+    <div style = "margin: 10px; padding: 10px; background-color: red"(click)="stopBubbling($event)">
+        <button (click)="buttonClick()">Bubble!</button>
+    </div>
+</div>
+<div style = "margin: 10px; padding: 10px; background-color: green" [id] = "getId()" (click)="clickDivId()">
+
+</div>
+<input type="text" #name>
+<button (click)="name.focus()">Focus the input</button>
+<button (click)="fokusPokus(name)">Focus the input</button>
+
+`,
+    standalone: true,
+    imports: [CommonModule],
+
 })
-export class RacesComponent {}
+export class RacesComponent {
+    races: Array<RaceModel> = [];
+    firstName: string = "";
+    lastName: string = "";
+    messageCount = "";
+
+
+    refreshRaces(): void {
+        this.races = [{ name: 'London' }, { name: 'Lyon' }];
+    }
+    onButtonClick(event: Event) {
+        console.log(event);
+    }
+    onSpacePress(): void {
+        console.log("Space")
+    }
+    onSpacePress1(event: Event): void {
+        console.log(event, "Space1")
+    }
+    bubbling() {
+        console.log("This is bubbling!!")
+    }
+    stopBubbling(event: Event) {
+        // event.preventDefault(); event.stopPropagation();
+        console.log("2")
+    }
+    buttonClick() {
+        console.log("1")
+    }
+    getId() {
+        console.log("myLoveId_console.log")
+        return "myLoveId"
+    }
+    clickDivId() {
+        console.log("Get Id")
+    }
+    fokusPokus(elem: HTMLInputElement) {
+        elem.focus()
+    }
+}
+
+interface RaceModel {
+    name: string
+}
