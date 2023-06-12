6cdde9a20871aa117c276b1fd62c277117242bf1
Tabalamari <tabalamari@gmail.com>
2023-06-12 09:38:12 +0100

    Other template directives

--- src/ponies.component.ts
+++ src/ponies.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'ns-ponies',
-    template: ``,
-    standalone: true, 
+    template: `
+    <div><button style = "margin-top: 20px" (click)="refreshPonies()">Refresh</button></div>
+    <ul>
+       <li *ngFor="let pony of ponies; even as isEven" [style.color] = "isEven ? 'green' : 'black'">{{ pony.name }}</li>
+    </ul>`,
+    standalone: true,
     imports: [CommonModule]
 })
-export class PoniesComponent { 
+export class PoniesComponent {
 
     ponies: Array<PonyModel> = [{ name: 'Rainbow Dash' }, { name: 'Pinkie Pie' }];
-
+    refreshPonies() {
+        this.ponies = [{ name: 'Fluttershy'}, {name: 'Rarity'}]
+    }
 }
 
 interface PonyModel { name: string }
