c3deda333462ebd61e6984b6762218069e9b1e07
Tabalamari <tabalamari@gmail.com>
2023-06-19 19:12:12 +0100

    Pipes, slice

--- src/ponies.component.ts
+++ src/ponies.component.ts
@@ -13,18 +13,23 @@ import { PonyComponent } from './pony.component';
     </ul>
     <div class="pink">Ffffff</div>
     <div>{{ ponies | slice:0:2 | json }}, "HERE ARE PONIES in an interpolation expression"</div>
-    
+
     <div *ngFor="let pony of ponies | slice : 0 : 2"> {{pony | json}}"HERE ARE PONIES in an property expression through ngFor"</div>
+    <div *ngFor="let pony of ponies | slice : 0 : size"> {{pony | json}}"HERE ARE PONIES in an property expression through ngFor with dynamic argument"</div>
+    <!-- <p>You can use this to create a dynamic display where your user chooses how many elements she/he wants to see.</p> -->
+    <div *ngFor="let pony of ponies | slice : 0 : 2 as total; index as i">
+        {{ i + 1 }}/{{ total.length }}: {{ pony.name }}
+    </div>
     <p [textContent]="ponies | json">"HERE ARE PONIES in a property expression"<p>
     <p>{{ 'Ninja Squad' | slice:0:5 }}</p>`,
     standalone: true,
     imports: [CommonModule, PonyComponent],
 })
 export class PoniesComponent {
-
-    ponies: Array<PonyModel> = [{ id: 1, name: 'Rainbow Dash' }, {id: 2, name: 'Pinkie Pie' }];
+    size = 2;
+    ponies: Array<PonyModel> = [{ id: 1, name: 'Rainbow Dash' }, { id: 2, name: 'Pinkie Pie' }];
     refreshPonies() {
-        this.ponies = [{ id: 3, name: 'Fluttershy'}, { id:4, name: 'Rarity'}]
+        this.ponies = [{ id: 3, name: 'Fluttershy' }, { id: 4, name: 'Rarity' }]
     }
 
     betOnPony(pony: PonyModel) {
